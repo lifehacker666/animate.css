16d83517e53e7f5a0cdf642921c03b56d8091bac
lifehacker666 <lifehacker666@gmail.com>
2014-10-30 14:28:16 +0300

    menu 2, sticky waipoint

--- common.js
+++ common.js
@@ -3,9 +3,31 @@
     /* Верхнее меню */
     var navLi = $('.menu-top ul > li > a');
 
-    $('.block').waypoint( function () {
-        var hash = $(this).attr('id');
+    // Выделение активного пункта при скролле
+    $('.block')
+        .waypoint( function (dir) {
+            var hash = $(this).attr('id');
 
+            if (dir === "down") { //скролл сверху вниз
+                menuItemSelect(navLi, hash);
+            }
+        }, {
+            offset: $('header').height()
+        })
+        .waypoint( function (dir) {
+            var hash = $(this).attr('id');
+
+            if (dir === "up") { //скролл снизу вверх
+                menuItemSelect(navLi, hash);
+            }
+        }, {
+            offset: function() {
+                return - $(this).outerHeight() + $('header').height();
+            }
+        });
+
+    //ф-я подставляет активному пункту класс selected
+    function menuItemSelect(navLi, hash){
         navLi.parent().removeClass('selected');
 
         $.each( navLi, function() {
@@ -13,25 +35,7 @@
                 $(this).parent().addClass('selected');
             }
         });
-    }, {
-        offset: $('header').height()
-    });
-
-    $('.block').waypoint( function () {
-        var hash = $(this).attr('id');
-
-        navLi.parent().removeClass('selected');
-
-        $.each( navLi, function() {
-            if ( $(this).attr('href').slice(1) === hash ){
-                $(this).parent().addClass('selected');
-            }
-        });
-    }, {
-        offset: function() {
-            return - $(this).outerHeight() + $('header').height();
-        }
-    });
+    }
 
     //плавный скролл
     navLi.click(function (e) {
@@ -43,6 +47,17 @@
         $('html, body').animate( { scrollTop: destination - offset }, 800 );
     });
     /* /Верхнее меню */
+
+
+    /* Прилипающий элемент */
+    if( $(".sticky-element").is("div") ){
+        $.getScript( '/waypoints-sticky.min.js', function() {
+
+            $('.sticky-element').waypoint('sticky');
+
+        });
+    }
+    /* /Прилипающий элемент */
 
     /* CSS3 анимации */
     //определяем анимации для всех анимируемых блоков
